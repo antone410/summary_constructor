5e88b8fab4885438f44c8aba084dbd581654ae9c
Mikhail Levakov <61646888+antone410@users.noreply.github.com>
2021-12-20 16:44:01 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Step1 from './components/Steps/Step1';
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Summary constructior</h1>
+      <h3>Step 1: Основаня информация</h3>
+      <Step1></Step1>
     </div>
   );
 }
